7e277f14543a1e63ea8a7d693a89f7054b0e417f
Keshia Angela Jardiel <kjardielschool@gmail.com>
2026-09-23 11:39:57 +0800

    Fix home view POST handling, templates, and everything

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
 
 def home(request):
     if request.method == 'POST':
         form = ListForm(request.POST or None)
+
         if form.is_valid():
             form.save()
             all_items = List.objects.all()
-            context = {'all_items': all_items}  # Fixed key from 'all.items' to 'all_items'
+            context = {'all_items' : all_items}
             return render(request, 'home.html', context)
-
+        else:
+            all_items = List.objects.all()
+            context = {'all_items' : all_items}
+            return render(request, 'home.html', context)
     else:
         all_items = List.objects.all()
         context = {'all_items' : all_items}
-        return render(request, 'home.html', context )
+        return render(request, 'home.html', context)
 
 def delete(request, list_id):
     item = List.objects.get(pk = list_id)
